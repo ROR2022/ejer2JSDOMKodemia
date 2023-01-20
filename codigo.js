6a48b39132b3bf718072cd49394c8e9d34b46adb
ROR2022 <rami.ror279@gmail.com>
2023-01-19 23:24:13 -0600

    nueva version

--- codigo.js
+++ codigo.js
@@ -1,34 +1,121 @@
-import { objetoRick } from "./api.js";
+//import { objetoRick } from "./api.js";
+
+let data = [];
+
+const url= 'https://rickandmortyapi.com/api/character';
+
+
+
+const getAllData = async ()=> {
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        data= [...result.results];
+        showCards(data);
+        return
+    } catch (error) {
+        console.log(error);
+        return
+    }
+}
+
+const showAll = document.querySelector('#showAll');
+showAll.className='btn btn-primary d-block ms-auto me-auto my-3'
+showAll.addEventListener('click',()=>{
+    getAllData();
+    showAll.classList.add('d-none');
+})
+
+
 
 const cardsContainer= document.querySelector('#containerCards');
 
+const showNewData = (id)=>{
+    const newData= data.filter((item)=>item.id!==id);
+    data=[...newData];
+    //console.log(newData);
+    cardsContainer.innerHTML='';
+    showCards(newData);
+}
 
-objetoRick.results.map((el)=>{
-    //console.log(el.name);
-    const name= document.createElement('h6');
-    name.textContent=el.name;
-    const id= document.createElement('p');
-    id.textContent=el.id;
-    const estatus = document.createElement('p');
-    estatus.textContent= el.status;
-    const especies = document.createElement('p');
-    especies.textContent= el.species;
-    const type = document.createElement('p');
-    type.textContent= el.type;
-    const gender= document.createElement('p');
-    gender.textContent= el.gender;
-    const image= document.createElement('img');
-    image.src= el.image;
+const showModalCard = ({image,name})=>{
+
+    const modalContainer = document.createElement('div');
+    modalContainer.className='miModalContainer';
+
+    const nameCharacter= document.createElement('h6');
+    nameCharacter.textContent=name;
+    nameCharacter.className='text-info fs-4 text-center mt-5';
+    const imageCharacter= document.createElement('img');
+    imageCharacter.src= image;
+    imageCharacter.className='rounded w-50 d-block ms-auto me-auto';
+    const deleteButon= document.createElement('button');
+    deleteButon.textContent='Ocultar';
+    deleteButon.className='btn btn-secondary m-3 d-block ms-auto me-auto';
+    deleteButon.addEventListener('click',(e)=>{
+        modalContainer.classList.add('d-none');
+    })
+
+    modalContainer.appendChild(nameCharacter);
+    modalContainer.appendChild(imageCharacter);
+    modalContainer.appendChild(deleteButon);
+
+
+    document.body.appendChild(modalContainer);
+
+    return
+}
+
+
+const makeCard = (personaje)=>{
+    const {name,id,image} = personaje;
 
     const tarjeta= document.createElement('div');
-    tarjeta.appendChild(image);
-    tarjeta.appendChild(id);
-    tarjeta.appendChild(name);
-    tarjeta.appendChild(estatus);
-    tarjeta.appendChild(especies);
-    tarjeta.appendChild(type);
-    tarjeta.appendChild(gender);
-    
-    tarjeta.className='border m-2 rounded border-2 text-center';
-    cardsContainer.appendChild(tarjeta);
-})
+    tarjeta.className='border m-2 rounded border-2 text-center p-2';
+    tarjeta.id=id;
+
+    const nameCharacter= document.createElement('h6');
+    nameCharacter.textContent=name;
+    nameCharacter.className='text-info fs-4';
+    const imageCharacter= document.createElement('img');
+    imageCharacter.src= image;
+    imageCharacter.className='rounded'
+    const deleteButon= document.createElement('button');
+    deleteButon.textContent='Eliminar';
+    deleteButon.className='btn btn-secondary m-3';
+    deleteButon.addEventListener('click',(e)=>{
+        showNewData(id);
+    })
+    const modalButton= document.createElement('button');
+    modalButton.textContent='modalCard';
+    modalButton.className='btn btn-primary m-3 d-block ms-auto me-auto';
+    modalButton.addEventListener('click',()=>{
+        showModalCard(personaje);
+    })
+
+    tarjeta.appendChild(imageCharacter);
+    tarjeta.appendChild(nameCharacter);
+    tarjeta.appendChild(modalButton);
+    tarjeta.appendChild(deleteButon);
+
+    return tarjeta
+}
+
+
+
+
+
+
+const showCards = (cards)=>{
+
+    cards.forEach((personaje)=>{
+        const tarjeta= makeCard(personaje);
+        cardsContainer.appendChild(tarjeta);
+    })
+
+}
+
+
+
+
+
